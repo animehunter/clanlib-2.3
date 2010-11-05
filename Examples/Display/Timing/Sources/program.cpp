6f9f04d88db2e2328c71ea86efb30c1d4d19d57d
rombust <rombust@cc39f7f4-b520-0410-a30f-b56705a9c917>
2010-11-05 08:13:29 +0000

    Modify the timing example use OpenGL, else it is not smooth on slow pc's

--- Examples/Display/Timing/Sources/program.cpp
+++ Examples/Display/Timing/Sources/program.cpp
@@ -31,9 +31,9 @@
 #include "timing.h"
 
 // Choose the target renderer
-//#define USE_OPENGL_2
+#define USE_OPENGL_2
 //#define USE_OPENGL_1
-#define USE_SOFTWARE_RENDERER
+//#define USE_SOFTWARE_RENDERER
 
 #ifdef USE_SOFTWARE_RENDERER
 #include <ClanLib/swrender.h>
