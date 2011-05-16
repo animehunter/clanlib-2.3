228eca336fd97d7b6018ffff7595ac0592f36489
mbn <mbn@cc39f7f4-b520-0410-a30f-b56705a9c917>
2011-05-16 07:53:08 +0000

    "The patch everyone wanted, but everyone was too lazy to add"

--- Sources/API/Core/Math/cl_math.h
+++ Sources/API/Core/Math/cl_math.h
@@ -41,6 +41,7 @@
 template<typename T> inline T cl_pow2(T value) { return value*value; }
 template<typename A, typename B> inline A cl_min(A a, B b) { return a < b ? a : b; }
 template<typename A, typename B> inline A cl_max(A a, B b) { return a > b ? a : b; }
+template<typename A, typename B, typename C> inline C cl_clamp(A minval, B maxval, C val) { return cl_max(minval, cl_min(maxval, val)); }
 
 /// \}
 
