9c3e0c74a64f30c132f353a4518978564031e062
rombust <rombust@cc39f7f4-b520-0410-a30f-b56705a9c917>
2011-07-18 14:24:03 +0000

    Modified the OpenCL example

--- Examples/OpenCL/Basic/Sources/basic.cpp
+++ Examples/OpenCL/Basic/Sources/basic.cpp
@@ -58,30 +58,99 @@ int Basic::start(const std::vector<CL_String> &args)
 	// Get the graphic context
 	CL_GraphicContext gc = window.get_gc();
 
-
 	cl_int error;
 	cl_platform_id platform;
 	cl_uint platforms;
 	
-	error=clGetPlatformIDs(1, &platform, &platforms);
+	error = clGetPlatformIDs(1, &platform, &platforms);
 	if (error != CL_SUCCESS)
 		throw CL_Exception("clGetPlatformIDs failed");
 
 	cl_device_id device;
 	cl_uint devices;
-	error=clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, &devices);
+	error = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, &devices);
 	if (error != CL_SUCCESS)
 		throw CL_Exception("clGetDeviceIDs failed");
-
-
+	
 	cl_context_properties properties[]={CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0};
 	cl_context context;
-	context=clCreateContext(properties, 1, &device, NULL, NULL, &error);
+	context = clCreateContext(properties, 1, &device, NULL, NULL, &error);
 	if (error != CL_SUCCESS)
 		throw CL_Exception("clCreateContext failed");
 
+	cl_command_queue cq;
+	cq = clCreateCommandQueue(context, device, 0, &error);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clCreateCommandQueue failed");
+
+	static const char source_code[] = 
+		""
+		"__kernel void simple_test(__global float *vals)"
+		"{"
+		"	const uint i = get_global_id(0);"
+		""
+		"vals[i] = 1.23;"
+		"}"
+		"";
+
+
+	const char *src[] = {source_code};
+
+	cl_program prog;
+	prog = clCreateProgramWithSource(context, 1, src, NULL, &error);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clCreateProgramWithSource failed");
+
+	error = clBuildProgram(prog, 0, NULL, "", NULL, NULL);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clBuildProgram failed");
+
+	cl_kernel k_simple_test;
+	k_simple_test = clCreateKernel(prog, "simple_test", &error);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clCreateKernel failed");
+
+	static const int buffer_length = 64;
+	cl_mem ref_buffer;
+	ref_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float) *  buffer_length, NULL, &error);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clCreateBuffer failed");
+
+	error = clSetKernelArg(k_simple_test, 0, sizeof(ref_buffer), &ref_buffer);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clSetKernelArg failed");
+
+	const size_t worksize = buffer_length;
+	error = clEnqueueNDRangeKernel(cq, k_simple_test, 1, NULL, &worksize, NULL, 0, NULL, NULL);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clEnqueueNDRangeKernel failed");
+
+	error = clFinish(cq);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clFinish failed");
+
+	float ram_buffer[buffer_length];
+	error=clEnqueueReadBuffer(cq, ref_buffer, CL_TRUE, 0, sizeof(float) * buffer_length, ram_buffer, 0, NULL, NULL);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clEnqueueReadBuffer failed");
+
+	error=clFinish(cq);
+	if (error != CL_SUCCESS)
+		throw CL_Exception("clFinish failed");
+
 	// Clean up
+	clReleaseKernel(k_simple_test);
+	clReleaseProgram(prog);
+	clReleaseCommandQueue(cq);
 	clReleaseContext(context);
+	clReleaseMemObject(ref_buffer);
+
+	// Test output
+	for (int cnt=0;cnt < buffer_length; cnt++)
+	{
+		if (ram_buffer[cnt] != 1.23f)
+			throw CL_Exception("It did not work!");
+	}
 
 	CL_Font font(gc, "tahoma", 24);
 
