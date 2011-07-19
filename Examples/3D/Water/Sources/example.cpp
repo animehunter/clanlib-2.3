ef319cee3502cc203274680605890f0f7fbdb832
rombust <rombust@cc39f7f4-b520-0410-a30f-b56705a9c917>
2011-07-19 14:18:01 +0000

    Fix water example for GCC

--- Examples/3D/Water/Sources/example.cpp
+++ Examples/3D/Water/Sources/example.cpp
@@ -202,8 +202,9 @@ void App::update_light(CL_GraphicContext &gc)
 
 	CL_Vec4f light_specular(0.5f, 0.5f, 0.5f, 1.0f);
 	CL_Vec4f light_diffuse(0.5f, 0.5f, 0.5f, 1.0f);
+	CL_Vec4f light_ambient( 0.3f, 0.3f, 0.3f, 1.0f);
 
-	scene.gs->shader_texture.SetLight(light_vector, light_specular, light_diffuse, CL_Vec4f( 0.3f, 0.3f, 0.3f, 1.0f) );
+	scene.gs->shader_texture.SetLight(light_vector, light_specular, light_diffuse, light_ambient );
 }
 
 void App::calculate_matricies(CL_GraphicContext &gc)
